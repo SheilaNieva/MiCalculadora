29f63d116ffb1d590893ab10618aa17d3f23a8e3
SheilaNieva <sheilanievalo.2000@gmail.com>
2021-04-15 15:37:04 +0200

    Boton clear y errores

--- app/src/main/java/com/example/micalculadora/MainActivity.kt
+++ app/src/main/java/com/example/micalculadora/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var firstNumber =0.0
@@ -62,7 +63,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             btMul -> onOperationPressed("x")
             btDiv -> onOperationPressed("/")
             btEqual -> onEqualPressed()
-            btClear -> ""
+            btClear -> onClearPressed()
         }
     }
 
@@ -108,10 +109,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         operation = null
         firstNumber = result.toString().toDouble()
 
-        screen.text = if(result.toString().endsWith(".0")){
+        try {
+
+            screen.text = if (result.toString().endsWith(".0")) {
                 result.toString().replace(".0", "")
-            }else {
-            "%.2f".format(result)
+            } else {
+                "%.2f".format(result)
+            }
+
+        }catch (e: Exception){
+            e.printStackTrace()
         }
+    }
+
+    private fun onClearPressed(){
+        screen.text= "0"
+        firstNumber = 0.0
+        secondNumber = 0.0
     }
 }
